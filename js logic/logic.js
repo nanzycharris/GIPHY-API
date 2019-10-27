1dc87fa5fe534e9f473d4db9c110e9cc72716121
Nancy Harris <nanzyharris@gmail.com>
2019-10-27 00:54:32 -0500

    files and folders added...

---
+++ js logic/logic.js
@@ -0,0 +1,8 @@
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
