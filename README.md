e709915f9722ca72cbb30d266027cd64a37fca0b
Nancy Harris <nanzyharris@gmail.com>
2019-10-27 03:48:08 -0500

    changes to read me, index, and css files...

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Giphy API - Homework 6
 
 This assignment uses the GIPHY API to create a dynamic web page that populates with gifs of popular sitcoms. This task is accomplished by call the GIPHY API and using JavaScript and jQuery to change the HTML.
 
+Instructions: 
+Click on the button of your prefered tv show and reminisce on its key comedical moments encapsulated in a Gif, or add a new title to the list by typing the name of your favorite sitcom.
+To animate the image, click on it.
+
 https://github.com/nanzycharris/GIPHY-API
--- assets/js logic/logic.js
+++ assets/js logic/logic.js
@@ -1,14 +1,14 @@
+// Render buttons function on page load
 $(document).ready(function () {
-
+    // Create variable holding array of initial search terms
     var sitcoms = [
         "The IT Crowd", "The Office", "Seinfeld", "Frasier", "Everybody Loves Raymond", "The Middle",
         "New Girl", "The Simpsons", "Bob's Burgers"
     ];
 
-    // function to make buttons and add to page
+    // Function to make buttons and add to page
     function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
         $(areaToAddTo).empty();
-
         for (var i = 0; i < arrayToUse.length; i++) {
             var a = $("<button>");
             a.addClass(classToAdd);
@@ -16,25 +16,31 @@ $(document).ready(function () {
             a.text(arrayToUse[i]);
             $(areaToAddTo).append(a);
         }
-
     }
-
+    // Create function to call gifs
+    // Empty current container of gifs
     $(document).on("click", ".sitcom-button", function () {
         $("#sitcoms").empty();
         $(".sitcom-button").removeClass("active");
         $(this).addClass("active");
-
+        // Create variable that grabs $(this).attr('data-name')
         var type = $(this).attr("data-type");
+        // Create variable for query url
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=1hdfqbCgYYcCQ61lluq8Hi9gwoTbQofJ&limit=10";
-
+        // Make ajax call
+        // URL
+        // GET method
         $.ajax({
             url: queryURL,
             method: "GET"
         })
+            // .then(function(response))
             .then(function (response) {
+                // Create variable to hold results
                 var results = response.data;
-
+                // Create for loop for response.data.length
                 for (var i = 0; i < results.length; i++) {
+                    // Use jQuery to create div to hold gif
                     var sitcomDiv = $("<div class=\"sitcom-item\">");
 
                     var rating = results[i].rating;
@@ -43,14 +49,17 @@ $(document).ready(function () {
 
                     var animated = results[i].images.fixed_height.url;
                     var still = results[i].images.fixed_height_still.url;
-
+                    // create variable with attributes holding:
                     var sitcomImage = $("<img>");
+                    // Create variable with attributes holding src:
                     sitcomImage.attr("src", still);
+                    // Variable with attributes holding datastate for data-still
                     sitcomImage.attr("data-still", still);
+                    // Variable with attributes holding datastate for data-animated
                     sitcomImage.attr("data-animate", animated);
                     sitcomImage.attr("data-state", "still");
                     sitcomImage.addClass("sitcom-image");
-
+                    // append/prepend to page
                     sitcomDiv.append(p);
                     sitcomDiv.append(sitcomImage);
 
@@ -72,15 +81,17 @@ $(document).ready(function () {
             $(this).attr("data-state", "still");
         }
     });
-
+    // Function for submit button - add sitcom
     $("#add-sitcom").on("click", function (event) {
         event.preventDefault();
+        // Set variable for search term using .val()
         var newSitcom = $("input").eq(0).val();
 
         if (newSitcom.length > 2) {
+            // push variable to array
             sitcoms.push(newSitcom);
         }
-
+        // Run render buttons function
         populateButtons(sitcoms, "sitcom-button", "#sitcom-buttons");
 
     });
